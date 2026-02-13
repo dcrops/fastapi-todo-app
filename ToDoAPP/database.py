bccb2b815c5f6ab284af9d7cd0d5a919eb5ae607
Dave C. <dave_cropper2003@yahoo.co.uk>
2026-02-13 15:55:09 +1100

    Updated the postgres database connection for the one in render.com

--- ToDoAPP/database.py
+++ ToDoAPP/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://admin:Pa$$w0rd@localhost:3306/todoapplicationdatabase'
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
